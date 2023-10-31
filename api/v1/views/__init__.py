c771c96b605f1be6ac4c71062f63d71f14590215
domodan <ongomdaniel9@gmail.com>
2023-10-31 11:10:43 -0700

    fixed the errors from pycode style

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,10 +4,6 @@
 """
 from flask import Flask
 from flask import Blueprint
-
-#create a variable app_views which is an instance of Blueprint (url prefix must be /api/v1)
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -16,3 +12,6 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.places_amenities import *
+
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
